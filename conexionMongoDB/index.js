94e9cf6f5f3cd0b79ded028c29d92fdf579a7551
JesusDigital <jesusdanielcv1105@gamil.com>
2024-10-10 15:35:18 -0400

    "mongoDB"

---
+++ conexionMongoDB/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+const port = process.env.PORT||9000;
+app.listen(port,()=> console.log('server listen on port',port))
+
+//GET,POST,DELETE,UPDATE
+//CRUD = CREATE,READ,UPDATE,DELETE
+app.get('/',(req,res)=>{
+    res.send('Bienvenido al servidor')
+})
+
+//conexion mongo db
+mongoose
+.connect(process.env.MONGODB_URI)
+.then(()=>console.log('Te has conectado a Mongodb'))
+.catch((error)=>console.error(error))
