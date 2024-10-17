1048e517ace2e7b7e4f33ad661a9abd9f0882d67
JesusDigital <jesusdanielcv1105@gamil.com>
2024-10-17 16:13:23 -0400

    actualizar PROYECTO_Servidor

---
+++ PROYECTO_Servidor/views/tareas/tareas.js
@@ -0,0 +1,28 @@
+const usuario = JSON.parse(localStorage.getItem('usuario'))
+const formulario = document.querySelector('#form-todos')
+const lista = document.querySelector('#todos-list')  
+const inputF = document.querySelector('#form-input')
+const cerrarBtn = document.querySelector('#cerrar-btn') 
+
+
+if(!usuario){
+    //no existe, no ha iniciado sesion
+    window.location.href = '/'
+}
+
+formulario.addEventListener('submit', async e=>{
+    e.preventDefault()
+    
+    const respuesta = await fetch('http://localhost:3000/usuarios',{
+        method:'POST',
+        headers: {
+            'Content-Type':'application/json'
+            //buscar cuales son los otros tipos de Content-Type
+        },
+        body:JSON.stringify({texto:inputF.value,nombre:usuario.nombre})
+    })
+    
+    //const users = await respuesta.json()
+
+    //const user = users.find(i=>i.nombre===loginInput.value)
+})
