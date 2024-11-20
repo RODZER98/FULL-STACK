a08aa7f942d632bedeb1361a44e3a96fb3e16e11
Rodney G <rodneygallardo98@gmail.com>
2024-11-20 16:28:13 -0400

    "lonuevo"

---
+++ adminPanelnuevo/js/api.js
@@ -0,0 +1,63 @@
+const url = 'http://localhost:3000/menu'
+
+const nuevoproducto = async producto =>{
+    try{
+        await fetch(url,{
+            method: 'POST',
+            body: JSON.stringify(producto),
+            headers:{
+                'Constent-Type':'application/json'
+            }
+        })
+    }catch(error){
+        console.log(error)
+    }
+}
+
+const obtenerproductos = async ()=>{
+    //me retorna listado de productos que encuentran en el endpoint
+    //localhost:3000
+    try{
+        const resultado = await fetch(url)
+        const productos = await resultado.json()
+        return productos
+    }catch(error){
+        console.log(error)
+    }
+}
+
+const obtenerproducto = async id=>{
+    //me retorna 1 producto que se encuentran
+    //en el endpoint de menu localhost:3000/menu/id dado un id
+    try{
+        const resultado = await fetch(`${url}/${id}`)
+        const producto = await resultado.json()
+    }catch(error){
+        console.log(error)
+    }
+}
+
+const editarproducto = async producto =>{
+    try{
+        const resultado = await fetch(`${url}/${producto.id}`,{
+            method:'PUT',
+            body: JSON.stringify(producto),
+            headers:{
+                'Constent-Type':'application/json'
+            }
+        })
+        const producto = await resultado.json()
+    }catch(error){
+        console.log(error)
+    }
+}
+
+const eliminarproducto = async id =>{
+    try{
+        await fetch(`${url}/${id}`,{
+            method:'DELETE'
+        })
+    }catch(error){
+        console.log(error)
+    }
+}
