7a1a07d7621473451cdd29c484d6d2119aa7fdaf
rodney gallardo <rodneygallardo98@gmail.com>
2024-11-30 21:05:12 -0400

    "cambiando"

--- fetchapi/js/cargarJSONArray.js
+++ fetchapi/js/cargarJSONArray.js
@@ -1,36 +1,36 @@
-const cargarJSONA = document.querySelector('#cargarJSONArray')
-cargarJSONA.addEventListener('click',obtenerDatosjsonA)
+const cargarJSONA = document.querySelector("#cargarJSONArray");
+cargarJSONA.addEventListener("click", obtenerDatosjsonA);
 
-function obtenerDatosjsonA(){
-    const url = 'data/empleados.js'
-    fetch(url)//conexion
-    .then(respuesta=>{
-        console.log(respuesta)
-        return respuesta.json()//conversion
-    }).then(datos=>{
-        console.log(datos)
-        mostrarHTMLA(datos)
-    }).catch(error=>{//error
-        console.log(error)
+function obtenerDatosjsonA() {
+  const url = "data/empleados.js";
+  fetch(url) //conexion
+    .then((respuesta) => {
+      console.log(respuesta);
+      return respuesta.json(); //conversion
     })
+    .then((datos) => {
+      console.log(datos);
+      mostrarHTMLA(datos);
+    })
+    .catch((error) => {
+      //error
+      console.log(error);
+    });
 }
 
-function mostrarHTMLA(data){
-    const contenido = document.querySelector('#contenido')
-    let texto = ''
-    data.forEach(i=>{
-        const{empresa,id,trabajo,nombre} = i
+function mostrarHTMLA(data) {
+  const contenido = document.querySelector("#contenido");
+  let texto = "";
+  data.forEach((i) => {
+    const { empresa, id, trabajo, nombre } = i;
 
     texto += `
     <p>Empleado: ${nombre}</p>
     <p>Id: ${id}</p>
     <p>Empresa: ${empresa}</p>
     <p>Trabajo: ${trabajo}</p>
-    `
-    })
+    `;
+  });
 
-    contenido.innerHTML = texto
-
-    
-    
+  contenido.innerHTML = texto;
 }
