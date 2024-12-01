a619804716c170a3308dea647088ef31252e2c80
rodney gallardo <rodneygallardo98@gmail.com>
2024-11-30 20:52:44 -0400

    "subiendo"

--- fetchapi/js/obtenertxt.js
+++ fetchapi/js/obtenertxt.js
@@ -1,17 +1,21 @@
-const cargartxtBtn = document.querySelector('#cargarTxt')
-cargartxtBtn.addEventListener('click',obtenerDatos)
+const cargartxtBtn = document.querySelector("#cargarTxt");
+cargartxtBtn.addEventListener("click", obtenerDatos);
 
-function obtenerDatos(){
-    const url = 'data/datos.txt'
-    fetch(url)//conexion
-    fetch('data/datos.txt').then(respuesta=>{
-        console.log(respuesta)
-        return respuesta.text()//conversion
-    }).then(datos=>{
-        console.log(datos)
-    }).catch(error=>{//error
-        console.log(error)
+function obtenerDatos() {
+  const url = "data/datos.txt";
+  fetch(url); //conexion
+  fetch("data/datos.txt")
+    .then((respuesta) => {
+      console.log(respuesta);
+      return respuesta.text(); //conversion
     })
+    .then((datos) => {
+      console.log(datos);
+    })
+    .catch((error) => {
+      //error
+      console.log(error);
+    });
 }
 
 //si 404 no lo encontro, si es 400 no hay conexion y si es 200 es que esta conectado
