873b652eec24e83296112508969b16aab4f10704
rodney gallardo <rodneygallardo98@gmail.com>
2024-11-30 20:26:33 -0400

    "listo"

--- APIJS/js/notificacion.js
+++ APIJS/js/notificacion.js
@@ -1,28 +1,26 @@
 //notificaciones nativas de JS
-const notificarBTN = document.querySelector('#notificar')
-const verNotiBTN = document.querySelector('#verNotificacion')
+const notificarBTN = document.querySelector("#notificar");
+const verNotiBTN = document.querySelector("#verNotificacion");
 
-notificarBTN.addEventListener('click',()=>{
-    //notificaciones API
+notificarBTN.addEventListener("click", () => {
+  //notificaciones API
 
-    Notification
-        .requestPermission()
-        .then(resultado=>{
-            console.log('El resultado es: ', resultado)
-        })
+  Notification.requestPermission().then((resultado) => {
+    console.log("El resultado es: ", resultado);
+  });
+});
 
-})
+verNotiBTN.addEventListener("click", () => {
+  console.log("ver");
+  console.log(Notification.permission);
+  if (Notification.permission === "granted") {
+    const notiVentana = new Notification("Aqui esta la notificacion", {
+      icon: "img/edetecnica.png",
+      body: "notificacion recibida",
+    });
 
-verNotiBTN.addEventListener('click',()=>{
-    console.log('ver')
-    if(Notification.permission === 'granted'){
-        const notiVentana = new Notification('Aqui esta la notificacion',{
-            icon:'img/edetecnica.png',
-            body:'notificacion recibida'
-        })
-
-        notiVentana.onclick = function(){
-            window.open('http://google.com')
-        }
-    }
-})
+    notiVentana.onclick = function () {
+      window.open("http://google.com");
+    };
+  }
+});
