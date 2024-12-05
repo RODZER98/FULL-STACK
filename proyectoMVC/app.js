9d8384a5784c6653240e4054915b8a793b5f2abf
Rodney G <rodneygallardo98@gmail.com>
2024-12-05 16:37:12 -0400

    "subiendocongitignore"

--- proyectoMVC/app.js
+++ proyectoMVC/app.js
@@ -2,13 +2,20 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const path = require('path')
 
 //conexion a BD
-(async()=>{
+
     try{
-        await mongoose.connect(process.env.MONGO_URL)
+        mongoose.connect(process.env.MONGO_URL)
         console.log('Conexion a BD correcta')
     } catch(error) {
         console.log(error)
     }
-})()
+
+
+
+//crear rutas de front end
+
+app.use('/', express.static(path.resolve('views','home')))
+module.exports.app
