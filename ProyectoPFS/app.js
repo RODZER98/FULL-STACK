8f11e9b4ebe4f943f6ddae4f31e5d74877617cee
Rodney Gallardo <rodzer@gmail.com>
2025-01-17 14:40:56 -0400

    "avanzamos_con_esto"

--- ProyectoPFS/app.js
+++ ProyectoPFS/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
@@ -6,8 +7,11 @@ const mongoose = require('mongoose');
 //conexion a la bd
 (async()=>{
     try{
-        await mongoose.connect()
+        await mongoose.connect(process.env.MONGO_URI_TEST);
+        console.log('Se conecto exitosamente a MongoDB');
     }catch(error){
         console.log(error);    
     }
-})
+})()
+
+module.exports = app;
